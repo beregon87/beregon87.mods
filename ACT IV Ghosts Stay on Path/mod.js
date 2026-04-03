6d154e5ae36044563edc14f769062c1ada6d3576
martin <beregon87@gmail.com>
2026-04-03 22:12:38 +0200

    Added more monster types

--- ACT IV Ghosts Stay on Path/mod.js
+++ ACT IV Ghosts Stay on Path/mod.js
@@ -1,18 +1,37 @@
-// monstats.txt
-const monstatsFilename = 'global\\excel\\monstats.txt';
-const monstats = D2RMM.readTsv(monstatsFilename);
+const act5FlyingMonsters = [
+    "willowisp5",   // Gloam
+    "willowisp6",   // Black Soul
+    "willowisp7",   // Burning Soul
+    "willowisp8",   // Burning Soul
 
-monstats.rows.forEach((row) => {
-  if (row["Id"] === "fingermage1") {
-    row["flying"] = "";
-  }
-});monstats.rows.forEach((row) => {
-  if (row["Id"] === "fingermage2") {
-    row["flying"] = "";
-  }
-});monstats.rows.forEach((row) => {
-  if (row["Id"] === "fingermage3") {
-    row["flying"] = "";
-  }
-});
-D2RMM.writeTsv(monstatsFilename, monstats);
+    "succubus6",    // Hell Temptress
+    "succubus7",    // Blood Temptress
+    "succubus8",    // Siren
+
+    "succubuswitch6",
+    "succubuswitch7",
+    "succubuswitch8",
+    "succubuswitch9"
+];
+
+// monstats.txt
+const monstatsFilenames = ['global\\excel\\monstats.txt', 'global\\excel\\base\\monstats.txt']
+
+monstatsFilenames.forEach(
+    (monstatsFilename) => {
+
+        const monstats = D2RMM.readTsv(monstatsFilename);
+
+        monstats.rows.forEach((row) => {
+            if (row["Id"].startsWith("fingermage")) {
+                row["flying"] = "";
+            } else if (row["Id"].startsWith("wraith")) {
+                row["BaseId"] = "flyingscimitar";
+                row["flying"] = "";
+            } else if (act5FlyingMonsters.includes(row["Id"])) {
+                row["flying"] = "";
+            }
+        });
+
+        D2RMM.writeTsv(monstatsFilename, monstats);
+    });
